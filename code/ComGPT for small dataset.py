6127a31bed34fe8581402966d537389e648e6d0c
shw <2540331911@qq.com>
2025-09-04 21:29:47 +0800

    Update ComGPT for small dataset.py

--- code/ComGPT for small dataset.py
+++ code/ComGPT for small dataset.py
@@ -40,13 +40,14 @@ iteration = 2 #迭代次数
 HaveSK = True #有SK的图编码
 WithoutSK = False #无SK的图编码
 promptselector = 5 #1-zeroshot 2-fewshot 3-cot 4-bag 5-nsg
+K = 10
 
 
 alllist = [1]
 
 
 for seed in alllist:
-    seed_list = GPTLCD.gpt_communityexpansion(seed,G,ns1,iteration,HaveSK,promptselector)
+    seed_list = GPTLCD.gpt_communityexpansion(seed,G,ns1,iteration,K,HaveSK,promptselector)
     for j in range(len(list_true)):
         flag = j
         if seed in list_true[j]:
@@ -54,4 +55,5 @@ for seed in alllist:
     a, b, c, d = GPTLCD.eval_scores(seed_list, list_true[flag])
 
     print("当前结果" + str(seed_list) +" F1:" + str(c)+" Jaccard:"+str(d))
+
 
