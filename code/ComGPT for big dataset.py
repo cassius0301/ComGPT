cd95e8cc9ba990f03d1f2f0a1a0e131f1484d3de
shw <2540331911@qq.com>
2026-01-02 14:09:33 +0800

    Update ComGPT for big dataset.py

--- code/ComGPT for big dataset.py
+++ code/ComGPT for big dataset.py
@@ -41,7 +41,7 @@ promptselector = 5 #1-zeroshot 2-fewshot 3-cot 4-bag 5-nsg
 K = 10
 
 for seed in alllist:
-    seed_list = GPTLCD.gpt_communityexpansion(seed,G,ns1,iteration,K,HaveSK,promptselector)
+    seed_list = GPTLCD.gpt_communityexpansion(seed,G,ns2,iteration,K,HaveSK,promptselector)
 
 
     realcommunity = []
@@ -60,4 +60,5 @@ for seed in alllist:
     sum = sum + c
     length = length + 1
     print("当前结果" + str(seed_list) + " F1:" + str(c) + " Jaccard:" + str(d))
+
 
